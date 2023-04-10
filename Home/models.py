b85471086713221e6d7d20e49c12fa56b2b3d4cd
PrabhasDasari <124909746+PrabhasDasari@users.noreply.github.com>
2023-04-10 19:28:58 +0530

    Update models.py

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class Register( models.Model ):
+    Name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    adress = models.CharField(max_length=20)
+    phone = models.CharField(
+        max_length=10,
+        validators=[RegexValidator('^[0-9]{10}$', _('country code is not required'))])
+    pinCode = models.CharField(
+        max_length=6,
+        validators=[RegexValidator('^[0-9]{6}$', _('Invalid postal code'))])
+    password = models.CharField(max_length=30)
+    
