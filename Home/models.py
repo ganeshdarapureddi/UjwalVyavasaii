a511d37ca4b4a53bd3eb6a3075f61db321aa7185
Avinash-Gurugubelli <avinashgurugubelli563@gmail.com>
2023-04-10 22:26:39 +0530

    updated models

--- Home/models.py
+++ Home/models.py
@@ -15,20 +15,20 @@ class Register( models.Model ):
     password = models.CharField(max_length=30)
 
 class Upload(models.Model):
-    P_Name=models.CharField(max_length=20)
+    pName=models.CharField(max_length=20)
     price=models.FloatField(max_length=5)
-    max_qty=models.FloatField(max_length=5)
-    d_time=models.DateTimeField()
+    maxQty=models.FloatField(max_length=5)
+    dTime=models.DateTimeField()
     phone=models.ForeignKey(Register,on_delete=models.CASCADE)
-    P_image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+    PImage = models.ImageField(default="default.jpg", upload_to="profile_pics")
 
 class Orders(models.Model):
-    mobile=models.ForeignKey(Register,on_delete=models.CASCADE,related_name='phone')
+    mobile=models.ForeignKey(Register,on_delete=models.CASCADE,related_name='+')
     quant=models.FloatField(max_length=5)
-    s_prc=models.FloatField(max_length=5)
-    a_time=models.DateTimeField()
-    P_Name=models.ForeignKey(Upload,on_delete=models.CASCADE,related_name='P_Name')
-    P_image=models.ForeignKey(Upload,on_delete=models.CASCADE,related_name='P_image')
+    sPrc=models.FloatField(max_length=5)
+    aTime=models.DateTimeField()
+    pName=models.ForeignKey(Upload,on_delete=models.CASCADE,related_name='+')
+    pImage=models.ForeignKey(Upload,on_delete=models.CASCADE,related_name='+')
 
 
     
