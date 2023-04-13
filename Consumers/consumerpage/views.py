bb056925e67cfe513b96d1c80171a5fbec24797d
PrabhasDasari <124909746+PrabhasDasari@users.noreply.github.com>
2023-04-13 21:00:58 +0530

    update

--- Consumers/consumerpage/views.py
+++ Consumers/consumerpage/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def funroute3(request):
-    return render(request,'consumerpage/consumer1.html')
+    return render(request,'consumerpage/consumerpage.html')
