ee38f28187c8564ca80dd0a8d6575e4ae3e7971e
Avinash-Gurugubelli <avinashgurugubelli563@gmail.com>
2023-04-14 11:33:42 +0530

    login page

--- Farmers/farmregister/views.py
+++ Farmers/farmregister/views.py
@@ -1,20 +1,10 @@
-
 from Home.models import Register
-from django.contrib.auth.forms import UserCreationForm  
-from django.contrib.auth import forms  
-from django.shortcuts import redirect, render  
-from django.contrib import messages  
-from django.contrib.auth.forms import UserCreationForm  
-from .forms import CustomUserCreationForm  
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render  ,HttpResponse
+from django.contrib import messages   
 
 def funroute3(request):
     if request.method=="POST":
-        form = UserCreationForm()  
-        if form.is_valid():  
-            form.save()
-        else:  
-            form = UserCreationForm()  
-            context = { 'form':form  }
         name=request.POST.get('name','')
         email=request.POST.get('email','')
         adress=request.POST.get('adress','')
@@ -25,7 +15,8 @@ def funroute3(request):
         pimg=request.POST.get('pimg','')
         fimg=request.POST.get('fimg','')
         cimg=request.POST.get('cimg','')
+        my_user=User.objects.create_user(name,email,password)
+        my_user.save()
         reg =Register(name=name,email=email,adress=adress,phone=phone,pinCode=pinCode,password=password,cpassword=cpassword,pimg=pimg,cimg=cimg,fimg=fimg)
         reg.save()
-    return render(request,'farmregister/farmregister1.html',context)
-    
+    return render(request,'farmregister/farmregister1.html')
