73898a44bc9400a3e92b67803656e4561d4ef302
Avinash-Gurugubelli <avinashgurugubelli563@gmail.com>
2023-04-16 09:36:32 +0530

    final

--- Consumers/consregister/views.py
+++ Consumers/consregister/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 
+from Home.models import Register
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render  ,HttpResponse
+from django.contrib import messages  
 
 def funroute3(request):
+    if request.method=="POST":
+        name=request.POST.get('name','')
+        email=request.POST.get('email','')
+        adress=request.POST.get('adress','')
+        phone=request.POST.get('phone','')
+        pinCode=request.POST.get('pinCode','')
+        password=request.POST.get('password','')
+        cpassword=request.POST.get('cpassword','')
+        pimg=request.POST.get('pimg','')
+        print(name,email,adress,phone,pinCode,password)
+        if password==cpassword:
+            my_user=User.objects.create_user(name,email,password)
+            my_user.save()
+            reg =Register(name=name,email=email,adress=adress,phone=phone,pinCode=pinCode,password=password,pimg=pimg,typ='customer')
+            reg.save()
+        else:
+            return HttpResponse("check password and confirm password")
     return render(request,'consregister/consregister11.html')
-
